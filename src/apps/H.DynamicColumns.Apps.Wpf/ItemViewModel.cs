e3ef3b9237e56e78a4790aeb4f2a035b0d4cda0d
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2021-10-23 11:28:52 +1000

    example: Implemented simple WPF example.

---
+++ src/apps/H.DynamicColumns.Apps.Wpf/ItemViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace H.DynamicColumns.Apps.Wpf;
+
+public class ItemViewModel
+{
+    public string Name { get; set; } = string.Empty;
+
+    public IReadOnlyCollection<string> Values { get; } = Enumerable
+        .Range(0, 100)
+        .Select(i => $"Value{i}")
+        .ToArray();
+}
--- src/apps/H.DynamicColumns.Apps.Wpf/MainWindow.xaml.cs
+++ src/apps/H.DynamicColumns.Apps.Wpf/MainWindow.xaml.cs
@@ -2,8 +2,33 @@
 
 public partial class MainWindow
 {
+    #region Properties
+
+    public List<DynamicColumn> DynamicColumns { get; } =  Enumerable
+        .Range(0, 100)
+        .Select(static i => new DynamicColumn
+        {
+            Header = $"Column{i}",
+            BindingPath = $"Values[{i}]",
+        })
+        .ToList();
+
+    public List<ItemViewModel> Items { get; } = Enumerable
+        .Range(0, 100)
+        .Select(static i => new ItemViewModel
+        { 
+            Name = $"Name{i}",
+        })
+        .ToList();
+
+    #endregion
+
+    #region Constructors
+
     public MainWindow()
     {
         InitializeComponent();
     }
+
+    #endregion
 }
